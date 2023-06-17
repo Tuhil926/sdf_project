7d5c40317417565aa7d35289b932c40a5c137f85
Aditya Garg <gargaditya2174@gmail.com>
2023-06-17 16:47:18 +0530

    added

---
+++ log_in/login.js
@@ -0,0 +1,10 @@
+
+
+let click = document.getElementById("sign_in"); 
+let bs = document.getElementById("pop_up");
+bs.style.display = "none";
+
+function show(){
+    bs.style.display="block";
+}
+

d3e103eb0787f1422e70bd922ffe6ece53c079ee
hemanthreddy6 <bonthumanihemanthreddy@gmail.com>
2023-06-23 10:18:58 +0530

    Added Basics of CSS slides

---
+++ Basics of CSS/Lesson 4/index.js
@@ -0,0 +1,17 @@
+createSlide();
+function createSlide()
+{
+    let box=$("#slide4");
+    box.css("width","100%");
+    let x=box.css("width").slice(0,-2);
+    // alert(x);
+    let a=x/30;
+    box.css("margin",a+"px");
+    box.css("height","548px");
+    x-=2*a;
+    box.css("width",x+"px");
+    x=548+2*a;
+    $("#main-container").css("height",x+"px");
+    box.css("background-color","#408E91");
+}
+// dark-mode: #2B2A4C

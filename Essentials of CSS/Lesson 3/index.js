dce8061a710f89ea533099becbc5246f0b0eba82
hemanthreddy6 <bonthumanihemanthreddy@gmail.com>
2023-06-28 00:43:26 +0530

    Added slide 3 of Essentials of CSS

--- Essentials of CSS/Lesson 3/index.js
+++ Essentials of CSS/Lesson 3/index.js
@@ -1,16 +1,16 @@
 createSlide();
 function createSlide()
 {
-    let box=$("#slide1");
+    let box=$("#slide3");
     box.css("width","100%");
     let x=box.css("width").slice(0,-2);
     // alert(x);
     let a=x/30;
     box.css("margin",a+"px");
-    box.css("height","848px");
+    box.css("height","648px");
     x-=2*a;
     box.css("width",x+"px");
-    x=848+2*a;
+    x=648+2*a;
     $("#main-container").css("height",x+"px");
     box.css("background-color","#408E91");
 }
